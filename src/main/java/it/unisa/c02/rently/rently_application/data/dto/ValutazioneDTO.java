a8d1c0de91331a4c5930a79cdd8f21936b0e2333
t.desimone5@studenti.unisa.it <tearosadesimone@gmail.com>
2024-01-16 22:54:21 +0100

    Update Gestione Valutazione controller and dto

--- src/main/java/it/unisa/c02/rently/rently_application/data/dto/ValutazioneDTO.java
+++ src/main/java/it/unisa/c02/rently/rently_application/data/dto/ValutazioneDTO.java
@@ -1,4 +1,4 @@
-package it.unisa.c02.rently.rently_application.data.DTO;
+package it.unisa.c02.rently.rently_application.data.dto;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -11,10 +11,11 @@ public class ValutazioneDTO {
 
     }
 
-    public ValutazioneDTO(int voto, String descrizione, long valutato) {
+    public ValutazioneDTO(int voto, String descrizione, long valutato, long valutatore) {
         this.voto = voto;
         this.descrizione = descrizione;
         this.valutato = valutato;
+        this.valutatore = valutatore;
     }
 
     private int voto;
@@ -22,5 +23,7 @@ public class ValutazioneDTO {
     private String descrizione;
 
     private long valutato;
+
+    private long valutatore;
 
 }

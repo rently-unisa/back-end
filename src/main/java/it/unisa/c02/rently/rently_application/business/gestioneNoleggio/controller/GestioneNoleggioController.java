5b3e13b86e90308e7e02b14624dada57881a4dcf
t.desimone5@studenti.unisa.it <tearosadesimone@gmail.com>
2024-01-19 17:56:49 +0100

    Update GestioneNoleggioController

--- src/main/java/it/unisa/c02/rently/rently_application/business/gestioneNoleggio/controller/GestioneNoleggioController.java
+++ src/main/java/it/unisa/c02/rently/rently_application/business/gestioneNoleggio/controller/GestioneNoleggioController.java
@@ -134,21 +134,26 @@ public class GestioneNoleggioController {
     @PostMapping("/aggiungi-noleggio")
     public ResponseEntity<String> aggiungiNoleggio(@RequestBody NoleggioDTO data){
 
-        Noleggio item = new Noleggio();
-        item.setStato(Noleggio.EnumStato.valueOf(data.getStato()));
-        item.setPrezzoTotale(data.getPrezzoTotale());
-        item.setDataInizio(data.getDataInizio());
-        item.setDataFine(data.getDataFine());
-        item.setDataRichiesta(data.getDataRichiesta());
-        item.setNoleggiante(areaPersonaleService.getDatiPrivati(data.getNoleggiante()));
-        item.setNoleggiatore(areaPersonaleService.getDatiPrivati(data.getNoleggiatore()));
-        item.setAnnuncio(annuncioService.getAnnuncio(data.getAnnuncio()).orElse(null));
+        List<Noleggio> list = noleggioService.checkDisponibilita(data.getAnnuncio(), data.getDataInizio(), data.getDataFine());
 
-        if(item.getNoleggiante() != null && item.getNoleggiatore()!= null && item.getAnnuncio() != null){
-            item = noleggioService.addNoleggio(item);
-            return responseService.Ok(item);
-        }
-        else
+        if(list == null) {
+
+            Noleggio item = new Noleggio();
+            item.setStato(Noleggio.EnumStato.valueOf(data.getStato()));
+            item.setPrezzoTotale(data.getPrezzoTotale());
+            item.setDataInizio(data.getDataInizio());
+            item.setDataFine(data.getDataFine());
+            item.setDataRichiesta(data.getDataRichiesta());
+            item.setNoleggiante(areaPersonaleService.getDatiPrivati(data.getNoleggiante()));
+            item.setNoleggiatore(areaPersonaleService.getDatiPrivati(data.getNoleggiatore()));
+            item.setAnnuncio(annuncioService.getAnnuncio(data.getAnnuncio()).orElse(null));
+
+            if (item.getNoleggiante() != null && item.getNoleggiatore() != null && item.getAnnuncio() != null) {
+                item = noleggioService.addNoleggio(item);
+                return responseService.Ok(item);
+            } else
+                return responseService.InternalError();
+        }else
             return responseService.InternalError();
     }
     @PostMapping("/salva-noleggio")

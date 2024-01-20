237ba6554f24bae6d5aafa3575c327adf947d432
rarissimaavis <r.fortino4@studenti.unisa.it>
2024-01-21 00:06:26 +0100

    Add JavaDoc for "GestioneRicerca" subsystem

--- src/main/java/it/unisa/c02/rently/rently_application/business/gestioneRicerca/service/GestioneRicercaService.java
+++ src/main/java/it/unisa/c02/rently/rently_application/business/gestioneRicerca/service/GestioneRicercaService.java
@@ -5,12 +5,55 @@ import it.unisa.c02.rently.rently_application.data.model.Annuncio;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * Questa interfaccia definisce le specifiche per i servizi dedicati alla gestione della ricerca di annunci
+ */
 public interface GestioneRicercaService {
 
+    /**
+     * Restituisce una lista di annunci che corrispondono alla categoria specificata.
+     *
+     * @param categoria Categoria degli annunci da cercare.
+     * @return Lista di annunci che appartengono alla categoria specificata.
+     */
     List<Annuncio> searchByCategoria(String categoria);
+
+    /**
+     * Restituisce una lista di annunci che corrispondono alla condizione specificata.
+     *
+     * @param condizione Condizione degli annunci da cercare.
+     * @return Lista di annunci che soddisfano la condizione specificata.
+     */
     List<Annuncio> searchByCondizione(String condizione);
+
+    /**
+     * Restituisce una lista di annunci che sono pubblicati tra le date specificate.
+     *
+     * @param inizio Data di inizio periodo di ricerca.
+     * @param fine   Data di fine periodo di ricerca.
+     * @return Lista di annunci pubblicati tra le date specificate.
+     */
     List<Annuncio> searchByData(Date inizio, Date fine);
+
+    /**
+     * Restituisce una lista di annunci che contengono la descrizione specificata.
+     *
+     * @param descrizione Descrizione da cercare negli annunci.
+     * @return Lista di annunci che contengono la descrizione specificata.
+     */
     List<Annuncio> searchByDescrizione(String descrizione);
+
+    /**
+     * Restituisce una lista di tutti gli annunci presenti nel sistema.
+     *
+     * @return Lista di tutti gli annunci presenti sulla piattaforma.
+     */
     List<Annuncio> searchAll();
+
+    /**
+     * Restituisce una lista di annunci di utenti premium.
+     *
+     * @return Lista di annunci di utenti premium presenti sulla piattaforma.
+     */
     List<Annuncio> searchAnnunciPremium();
 }
--- src/main/java/it/unisa/c02/rently/rently_application/business/gestioneRicerca/service/GestioneRicercaServiceImpl.java
+++ src/main/java/it/unisa/c02/rently/rently_application/business/gestioneRicerca/service/GestioneRicercaServiceImpl.java
@@ -8,37 +8,62 @@ import org.springframework.stereotype.Service;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * Implementazione del servizio di gestione della ricerca di annunci.
+ * Questa classe fornisce implementazioni concrete per i metodi dichiarati nell'interfaccia GestioneRicercaService.
+ */
 @Service
 @RequiredArgsConstructor
 public class GestioneRicercaServiceImpl implements GestioneRicercaService {
 
+    /**
+     * Istanza di GestioneAnnuncioDAO utilizzata per l'accesso ai dati degli annunci.
+     */
     private final GestioneAnnuncioDAO annuncioDAO;
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public List<Annuncio> searchByCategoria(String categoria) {
         return annuncioDAO.findByCategoria(categoria);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public List<Annuncio> searchByCondizione(String condizione) {
         return annuncioDAO.findByCondizione(condizione);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public List<Annuncio> searchByData(Date inizio, Date fine) {
         return annuncioDAO.findByDataFineBetween(inizio, fine);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public List<Annuncio> searchByDescrizione(String descrizione) {
         return annuncioDAO.findByDescrizioneContains(descrizione);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public List<Annuncio> searchAll() {
         return annuncioDAO.findAll();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public List<Annuncio> searchAnnunciPremium() {
         return annuncioDAO.findAllPremium();

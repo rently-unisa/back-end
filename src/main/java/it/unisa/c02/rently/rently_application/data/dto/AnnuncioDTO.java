55da28eaacc02fa50c272a4bb921afd1065cf1e0
t.desimone5@studenti.unisa.it <tearosadesimone@gmail.com>
2024-01-22 03:23:11 +0100

    Add JavaDoc for DTOs

--- src/main/java/it/unisa/c02/rently/rently_application/data/dto/AnnuncioDTO.java
+++ src/main/java/it/unisa/c02/rently/rently_application/data/dto/AnnuncioDTO.java
@@ -6,34 +6,88 @@ import lombok.Setter;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
-
+/**
+ * Questa classe rappresenta un DTO per un annuncio.
+ */
 @Getter
 @Setter
 public class AnnuncioDTO implements Serializable {
 
+    /**
+     * Rappresenta l'ID univoco di un annuncio.
+     */
     private long id;
+
+    /**
+     * Rappresenta il nome dell'annuncio.
+     */
     private String nome;
 
+    /**
+     * Rappresenta la strada dove ritirare l'oggetto dell'annuncio.
+     */
     private String strada;
 
+    /**
+     * Rappresenta la cottà dove si trova l'oggetto dell'annuncio.
+     */
     private String citta;
 
+    /**
+     * Rappresenta il Codice di Avviamento Postale (CAP) dell'annuncio.
+     */
     private String cap;
 
+    /**
+     * Rappresenta la descrizione dettagliata dell'annuncio.
+     */
     private String descrizione;
 
+    /**
+     * Rappresenta il prezzo di noleggio giornaliero dell'annuncio.
+     */
     private BigDecimal prezzo;
 
+    /**
+     * Rappresenta l'URL dell'immagine dell'oggetto dell'annuncio.
+     */
     private String immagine;
 
+    /**
+     * Rappresenta la categoria dell'oggetto dell'annuncio.
+     */
     private String categoria;
 
+    /**
+     * Rappresenta la condizione dell'oggetto dell'annuncio
+     */
     private String condizione;
 
+    /**
+     * Rappresenta la data di fine disponibilità dell'oggetto dell'annuncio.
+     */
     private String dataFine;
 
+    /**
+     * Rappresenta l'utente che ha pubblicato l'annuncio.
+     */
     private Long idUtente;
 
+
+    /**
+     * Costruttore per la creazione di un nuovo annuncio.
+     * @param nome Nome dell'annuncio.
+     * @param strada Nome della strada dove ritirare l'oggetto dell'annuncio.
+     * @param citta Città dove si trova l'oggetto dell'annuncio.
+     * @param cap Codice di Avviamento Postale (CAP) dell'annuncio.
+     * @param descrizione Descrizione dettagliata dell'oggetto dell'annuncio.
+     * @param prezzo Prezzo di noleggio giornaliero dell'oggetto dell'annuncio.
+     * @param immagine URL dell'immagine rappresentativa dell'oggetto dell'annuncio.
+     * @param categoria Categoria dell'oggetto dell'annuncio.
+     * @param condizione Condizione dell'oggetto dell'annuncio.
+     * @param dataFine Data di fine disponibilità dell'oggetto dell'annuncio.
+     * @param idUtente Utente che ha pubblicato l'annuncio.
+     */
     public AnnuncioDTO(long id, String nome, String strada, String citta, String cap, String descrizione, BigDecimal prezzo, String immagine, String categoria, String condizione, String dataFine, Long idUtente) {
         this.id = id;
         this.nome = nome;
@@ -49,9 +103,17 @@ public class AnnuncioDTO implements Serializable {
         this.idUtente = idUtente;
     }
 
+    /**
+     * Costruttore senza argomenti.
+     */
     public AnnuncioDTO() {
     }
 
+    /**
+     * Converte un Annuncio in un AnnuncioDTO.
+     * @param a Annuncio da convertire in DTO
+     * @return il DTO convertito dall'annuncio
+     */
     public AnnuncioDTO convertFromModel(Annuncio a) {
         AnnuncioDTO item = new AnnuncioDTO();
         item.setId(a.getId());
@@ -70,6 +132,12 @@ public class AnnuncioDTO implements Serializable {
         return item;
     }
 
+
+    /**
+     * Crea il path dell'immagine dell'annuncio passato in input
+     * @param a Annuncio a cui appartiene l'immagine
+     * @param serverPath il path assoluto del server
+     */
     public void setServerImage(Annuncio a, String serverPath) {
         //Path path = Paths.get(serverPath, "annunci", String.valueOf(a.getId()), a.getImmagine());
         String path = String.format("%s/%s/%s/%s", serverPath, "annunci", String.valueOf(a.getId()), a.getImmagine());
--- src/main/java/it/unisa/c02/rently/rently_application/data/dto/NoleggioDTO.java
+++ src/main/java/it/unisa/c02/rently/rently_application/data/dto/NoleggioDTO.java
@@ -6,36 +6,79 @@ import lombok.Setter;
 
 import java.math.BigDecimal;
 
-
+/**
+ * Questa classe rappresenta un noleggio sulla piattaforma.
+ */
 @Getter
 @Setter
 public class NoleggioDTO {
 
-
+    /**
+     * Rappresenta l'ID univoco di un noleggio.
+     */
     private Long id;
 
+    /**
+     * Rappresenta lo stato del noleggio.
+     */
     private String stato;
 
+    /**
+     * Rappresenta il prezzo totale del noleggio.
+     */
     private BigDecimal prezzoTotale;
 
+    /**
+     * Rappresenta la data di inizio del noleggio.
+     */
     private String dataInizio;
 
+    /**
+     * Rappresenta la data di fine del noleggio.
+     */
     private String dataFine;
 
+    /**
+     * Rappresenta la data di richiesta del noleggio.
+     */
     private String dataRichiesta;
 
+    /**
+     * Rappresenta l'utente che richiede il noleggio.
+     */
     private Long noleggiante;
 
+    /**
+     * Rappresenta l'utente che possiede l'oggetto del noleggio.
+     */
     private Long noleggiatore;
 
+    /**
+     * Rappresenta l'annuncio associato al noleggio.
+     */
     private Long annuncio;
 
+    /**
+     * Indica se è presente una specifica valutazioneUtente per un certo noleggio.
+     */
     private boolean valutazioneAlNoleggiante;
 
+    /**
+     * Indica se è presente una specifica valutazioneUtente per un certo noleggio.
+     */
     private boolean valutazioneAlNoleggiatore;
 
+    /**
+     * Indica se è presente una specifica valutazioneOggetto per un certo noleggio.
+     */
     private boolean valutazioneAnnuncio;
 
+
+    /**
+     * Converte un Noleggio in un NoleggioDTO.
+     * @param n Noleggio da convertire in DTO
+     * @return il DTO convertito dal Noleggio
+     */
     public NoleggioDTO convertFromModel(Noleggio n) {
         NoleggioDTO item = new NoleggioDTO();
         item.setId(n.getId());
